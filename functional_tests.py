a40fba66816722dd16b26d52122a9f4c30bdf52d
Andrey Starozhilov <andrey.starozhilov@mail.ru>
2020-04-26 22:47:25 +0500

    Add unittest functionality

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """Test new visitor."""
+
+    def setUp(self):
+        """Install."""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """After tests."""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """Check that you can create a list with tasks and get it later."""
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('The end of test')
+
+
+if __name__ == '__main__':
+    unittest.main()
