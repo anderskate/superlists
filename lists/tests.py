72382e81be921218c86c4e74268657189b51bc96
Andrey Starozhilov <andrey.starozhilov@mail.ru>
2020-04-27 14:28:05 +0500

    Create base view which return a minimal html

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 
@@ -10,5 +12,14 @@ class HomePageTest(TestCase):
         """Check that main url convert to main page view."""
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        """The home page return a correct html"""
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
 
 
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home_page(request):
     """View for home page."""
-    pass
+    return HttpResponse('<html><title>To-Do lists</title></html>')
 
