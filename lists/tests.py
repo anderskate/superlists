649e67a177e4a98ff861e5427681c44b96143895
Andrey Starozhilov <andrey.starozhilov@mail.ru>
2020-04-27 14:07:33 +0500

    Create first module test and url resolve

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,14 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
+    """Test home page."""
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        """Check that main url convert to main page view."""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
 
