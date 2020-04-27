842c70eef1bb1a8a6b20374c65586955d2a00a16
Andrey Starozhilov <andrey.starozhilov@mail.ru>
2020-04-27 18:46:51 +0500

    Refactor home page view

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -8,18 +9,7 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     """Test home page."""
 
-    def test_root_url_resolves_to_home_page_view(self):
-        """Check that main url convert to main page view."""
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
         """The home page return a correct html"""
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
-
-
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home_page(request):
     """View for home page."""
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
 
