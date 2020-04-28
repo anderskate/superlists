0e0bdd26879e6d31aefbab5e6591d9e09ef88b71
Andrey Starozhilov <andrey.starozhilov@mail.ru>
2020-04-29 00:06:07 +0500

    Add test that a string in list table exist

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,12 @@ class NewVisitorTest(unittest.TestCase):
         """After tests."""
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """Confirmation a string in the list table."""
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """Check that you can create a list with tasks and get it later."""
         self.browser.get('http://localhost:8000')
@@ -28,14 +34,18 @@ class NewVisitorTest(unittest.TestCase):
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-
         inputbox.send_keys('Buy a peals')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy a peals')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy a peals', [row.text for row in rows])
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy a pen')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: Buy a peals')
+        self.check_for_row_in_list_table('2: Buy a pen')
 
         self.fail('The end of test')
 
