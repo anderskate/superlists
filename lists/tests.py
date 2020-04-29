8f0dd61b42eb26df66d852f4bc60765cdd68ab07
Andrey Starozhilov <andrey.starozhilov@mail.ru>
2020-04-29 17:48:07 +0500

    Implement endpoint for creating new list

--- lists/tests.py
+++ lists/tests.py
@@ -16,26 +16,6 @@ class HomePageTest(TestCase):
 
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_post_request(self):
-        """Check that you can save post request."""
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_post(self):
-        """Check that after post request redirect is successful."""
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/one_list_in_the_world/')
-
-    def test_only_saves_items_when_necessary(self):
-        """Check that elements saves when only need it."""
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ItemModelTest(TestCase):
     """Test element list in model."""
@@ -76,5 +56,26 @@ class ListViewSet(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
+    """Test new list"""
+
+    def test_can_save_a_post_request(self):
+        """Check that you can save post request."""
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_post(self):
+        """Check that after post request redirect is successful."""
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'},
+        )
+
+        self.assertRedirects(response, '/lists/one_list_in_the_world/')
 
 
--- lists/views.py
+++ lists/views.py
@@ -5,9 +5,6 @@ from lists.models import Item
 
 def home_page(request):
     """View for home page."""
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/one_list_in_the_world/')
     return render(request, 'home.html')
 
 
@@ -15,3 +12,9 @@ def view_list(request):
     """View for a list representation."""
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    """New list"""
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/one_list_in_the_world/')
