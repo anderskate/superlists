af589a4c47b4b84b58cded730158ef6a2b17d403
Andrey Starozhilov <andrey.starozhilov@mail.ru>
2020-04-29 14:14:36 +0500

    Make functional_tests an application

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     """Test new visitor."""
 
     def setUp(self):
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """Check that you can create a list with tasks and get it later."""
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -49,6 +49,3 @@ class NewVisitorTest(unittest.TestCase):
 
         self.fail('The end of test')
 
-
-if __name__ == '__main__':
-    unittest.main()
