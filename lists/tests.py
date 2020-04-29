070bfe7ab989d26a137552eb0e7fc90cc5006162
Andrey Starozhilov <andrey.starozhilov@mail.ru>
2020-04-29 16:55:15 +0500

    Implement display lists

--- lists/tests.py
+++ lists/tests.py
@@ -29,22 +29,12 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/one_list_in_the_world/')
 
     def test_only_saves_items_when_necessary(self):
         """Check that elements saves when only need it."""
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_displays_all_list_items(self):
-        """Check that all list items displays."""
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -67,5 +57,24 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewSet(TestCase):
+    """Test representation a list"""
+
+    def test_uses_list_template(self):
+        """Check that is used list template."""
+        response = self.client.get('/lists/one_list_in_the_world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        """Check that all list items displays."""
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/one_list_in_the_world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
 
 
--- lists/views.py
+++ lists/views.py
@@ -7,6 +7,11 @@ def home_page(request):
     """View for home page."""
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/one_list_in_the_world/')
+    return render(request, 'home.html')
+
+
+def view_list(request):
+    """View for a list representation."""
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
