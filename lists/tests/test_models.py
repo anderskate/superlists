bfc165e8e7c6099e236b7297b49be1258775c5d3
Andrey Starozhilov <andrey.starozhilov@mail.ru>
2020-06-02 17:04:14 +0500

    Add get_absolute_url method

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -41,3 +41,9 @@ class ListAndItemModelTest(TestCase):
         with self.assertRaises(ValidationError):
             item.save()
             item.full_clean()
+
+    def test_get_absolute_url(self):
+        """Test that get absolute url."""
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+
